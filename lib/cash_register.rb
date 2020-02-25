4a224f3c3663ff51fe880e674224a0ff4f156361
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-25 23:04:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,6 +8,7 @@ class CashRegister
     @items = []
   end
 
+<<<<<<< HEAD
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
     quantity.times { self.items << title }
@@ -29,3 +30,30 @@ class CashRegister
     self.total = self.total - @last_item
   end 
 end
+=======
+  
+
+  def add_item(title, price, quantity=1)
+    @total += price * quantity 
+    quantity.times do 
+      @items << title 
+    end 
+    @last_item = price * quantity 
+  end 
+  
+  def apply_discount
+    if self.total == 0
+      "There is no discount to apply." 
+    else 
+      @discount = 1-@discount/100.to_f
+      @total = @total * @discount
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def void_last_transaction
+     @items.delete_at(-1)
+    self.total = self.total - @last_transaction
+   end
+end
+>>>>>>> ec2216be7548b336310ee17ee0d9f0cd0982e916
